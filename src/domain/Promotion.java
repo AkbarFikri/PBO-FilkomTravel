cfe2ae4b82d340e862ddd3a5ae44d2df78c7ee8d
AkbarFikri <akbarfikriabdillah@gmail.com>
2024-05-05 20:08:34 +0700

    feat: ading implementation to Promotion class

--- src/domain/Promotion.java
+++ src/domain/Promotion.java
@@ -1,5 +1,5 @@
 package domain;
 
-public abstract class Promotion {
+public abstract class Promotion implements Applicable, Comparable<Promotion>{
     // TODO make a abstract class for Promotion
 }
--- src/entity/promotion/PercentOffPromo.java
+++ src/entity/promotion/PercentOffPromo.java
@@ -3,4 +3,38 @@ package entity.promotion;
 import domain.Promotion;
 
 public class PercentOffPromo extends Promotion {
+    @Override
+    public boolean isCustomerEligible(Object x) {
+        return false;
+    }
+
+    @Override
+    public boolean isMinimumPriceEligible(Object x) {
+        return false;
+    }
+
+    @Override
+    public boolean isShippingFeeEligible(Object x) {
+        return false;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getTotalCashback() {
+        return 0;
+    }
+
+    @Override
+    public double getTotalShippingFee() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Promotion o) {
+        return 0;
+    }
 }
